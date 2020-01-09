e21387ba4d4a8044a012a55f2cd233dbea5c473f
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-01-09 12:07:03 -0800

    Switched from nodemailer to Twilio SendGrid

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail');
 const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT;
-const user = process.env.USER;
-const pass = process.env.PASS;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post('/send', (req, res) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user,
-      pass
-    }
-  });
-
-  if(!req.body.name) {
-    return res.status(400).json({message: 'The name field is required!'});
-  }
-  if(!req.body.email) {
-    return res.status(400).json({message: 'The email field is required!'});
-  }
-  if(!req.body.message) {
-    return res.status(400).json({message: 'The message field is required!'});
-  }
-
-  const mailOptions = {
-    from: 'mohammadtourjportfolio@gmail.com',
-    to: 'mtourjoman0@gmail.com',
-    subject: 'Portfolio Message',
-    text: `
-    Message recieved from: ${req.body.name}
-    Email: ${req.body.email}
-    Company: ${req.body.company}
-    Message:
-    ${req.body.message}
-    `
-  };
+app.post('/send', async (req, res) => {
   
-  transporter.sendMail(mailOptions, (error, info) => {
-    if(error){
-      console.log(error);
-      return res.status(500).json({message: 'There was an internal server error while trying to send this message.'})
-    } else {
-      console.log('Email sent: ' + info.response);
-      return res.sendStatus(200)
-    }
-  });
+  try {
 
+    if(!req.body.name) {
+      return res.status(400).json({message: 'The name field is required!'});
+    }
+    if(!req.body.email) {
+      return res.status(400).json({message: 'The email field is required!'});
+    }
+    if(!req.body.message) {
+      return res.status(400).json({message: 'The message field is required!'});
+    }
+    sgMail.setApiKey(process.env.API_KEY);
+    const msg = {
+      to: process.env.RECIPIENT,
+      from: `Portfolio Website <${req.body.email}>`,
+      subject: 'Portfolio Message',
+      html: `
+      <strong>Name: </strong> ${req.body.name}<br>
+      <strong>Email: </strong> ${req.body.email}<br>
+      <strong>Company: </strong> ${req.body.company}<br>
+      <strong>Message:</strong><br>
+      ${req.body.message}
+      `,
+    };
+    await sgMail.send(msg);
+  
+    return res.sendStatus(200);
+  } catch (err) {
+    console.log(req.body);
+    return res.status(500).json({message: err.message});
+  }
   
 });
 
