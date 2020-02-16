8e6cf2af686d309d097e3344fa08dbdc739ecdf1
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-02-16 12:10:47 -0800

    Add missing project model

---
+++ models/Project.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  github: {
+    type: String,
+    required: true
+  },
+  demo: {
+    type: String,
+    required: false
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  gif: {
+    type: String,
+  },
+  mobile_thumbnail: {
+    type: String,
+  },
+  mobile_gif: {
+    type: String,
+  }, 
+  description: {
+    type: String,
+    required: true
+  },
+  tech: {
+    type: String,
+    required: true
+  },
+  early: {
+    type: Boolean,
+    default: false
+  },
+})
+
+module.exports = mongoose.model('Project', ProjectSchema, 'Projects')
